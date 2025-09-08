4b45b39fa05e79d6ec97d9527780b6009be198ca
Hammadabbasoffc <hammadabbasoffc@gmail.com>
2025-09-08 17:23:35 +0500

    first commit

---
+++ 01_class.js
@@ -0,0 +1,87 @@
+const num1 = 12
+const num2 = 7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(`Addition: ${num1} + ${num2} = ${num1 + num2}`);
+
+// Write a program that finds the largest among three numbers.
+
+
+
+// Write a program that checks if a number is even or odd.
+
+
+
+
+// Write a program to simulate a traffic light system:
+
+// If light = "red" → print "Stop".
+
+// If light = "yellow" → print "Get Ready".
+
+// If light = "green" → print "Go".
+
+// Otherwise → "Invalid signal".
+
+
+// const light = "yellow";
+
+
+
+// if(light === "red"){
+//     console.log("Stop");
+// }else if(light === "yellow"){
+//     console.log("Get Ready");
+// }else if(light === "green"){
+//     console.log("Go");
+// }else{
+//     console.log("Invalid signal");
+// }
+
+
+// const temperature = 55;
+
+// const reult =  55>= 55
+
+// if(temperature >= 55){
+//     console.log("On Ac at 16");
+// }else if(temperature < 55 && temperature >= 20){
+//     console.log("No Ac");
+// }
+
